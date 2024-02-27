e769409bcc21f649f2e3a4755ef6b786564cb2f4
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-02-27 17:20:52 +0900

    Spring Boot 애플리케이션에 의존성 추가 및 코드 변경*** ***스프링 부트 액추에이터 및 개발 도구, 프로젝트 롬복 의존성 추가*** ***Course 클래스에 필드 및 toString 메서드 추가*** ***CourseController 클래스에 retrieveAllCourses 메서드 추가*** ***application.properties 파일에 로깅 레벨 및 프로파일 설정 추가

--- src/main/java/com/in28minutes/springboot/learnspringboot/Course.java
+++ src/main/java/com/in28minutes/springboot/learnspringboot/Course.java
@@ -1,2 +1,23 @@
-package com.in28minutes.springboot.learnspringboot;public class Course {
+package com.in28minutes.springboot.learnspringboot;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Course {
+    private int id;
+    private String name;
+    private String author;
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
+++ src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -1,2 +1,18 @@
-package com.in28minutes.springboot.learnspringboot;public class CourseController {
+package com.in28minutes.springboot.learnspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @GetMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(new Course(1,"Learn AWS","in28minutes"),
+                new Course(2,"Learn Full stack with Spring Boot and Angular","in28minutes"),
+                new Course(3,"Hungry","in28minutes"),
+                new Course(4,"Master Microservices with Spring Boot and Spring Cloud","in28minutes"));
+    }
 }
